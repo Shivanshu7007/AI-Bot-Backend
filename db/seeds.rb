f897bdf1758e825658dbcc5a8c2bee3a01813dc4
Shivanshu Mishra <shivanshumishra@Shivanshus-MacBook-Air.local>
2026-02-21 15:20:28 +0530

    Add production admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# db/seeds.rb
+
+AdminUser.find_or_create_by!(email: "admin@example.com") do |admin|
+  admin.password = "password"
+  admin.password_confirmation = "password"
+end
+
+puts "Default admin ensured"
